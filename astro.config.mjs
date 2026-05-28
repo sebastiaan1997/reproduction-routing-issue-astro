061de833e80545d0eeeae7c06b03ef56e0a99789
Sebastiaan Saarloos <sebastiaan.saarloos@gmail.com>
2026-05-28 10:22:59 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: vercel(),
+  experimental: {
+    advancedRouting: {
+      fetchFile: 'router.ts'
+    }
+  }
+});
